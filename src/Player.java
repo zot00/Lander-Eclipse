7d5978aac523b5c21665727b788e2e9de49bec59
zot00 <league@WorthyMini-5.attlocal.net>
2018-12-15 15:56:11 -0800

    Started working on lander in eclipse

---
+++ src/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+	public static double y;
+	public static double x;
+	static double incrementer = Gravity.Gincrementer * 1.75;
+
+	public static void main(String[] args) {
+		if (Thrust.ythrust >= incrementer) {
+			Thrust.ythrust = incrementer;
+		}
+		if (Gravity.IsLanded == false) {
+			Gravity.gravity += Gravity.gravityCoefficient;
+			y += Gravity.gravity;
+			y -= Thrust.ythrust;
+			x += Thrust.xthrust;
+		}
+	}
+}
---
+++ src/Thrust.java
@@ -0,0 +1,5 @@
+
+public class Thrust {
+	static double ythrust;
+	static double xthrust;
+}
