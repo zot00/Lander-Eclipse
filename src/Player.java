e1f58d5a28cbfd06737dc877dbf0e67b2e969a1b
GT <league@WTS14-4.attlocal.net>
2018-12-29 11:29:04 -0800

    IDK, TBH

--- src/Player.java
+++ src/Player.java
@@ -4,14 +4,15 @@ public class Player {
 	static double incrementer = Gravity.Gincrementer * 1.75;
 
 	public static void main(String[] args) {
+
 		if (Thrust.ythrust >= incrementer) {
 			Thrust.ythrust = incrementer;
 		}
-		if (Gravity.IsLanded == false) {
+		if (Gravity.isLanded == false) {
 			Gravity.gravity += Gravity.gravityCoefficient;
-			y += Gravity.gravity;
-			y -= Thrust.ythrust;
-			x += Thrust.xthrust;
+			y -= Gravity.gravity;
+			y += Thrust.ythrust;
+			x -= Thrust.xthrust;
 		}
 	}
 }
